e7511a45a72195a3e877c34a2a96f6b2c7bb5f4b
ak-matsu <sandbox.matsu@gmail.com>
2021-05-09 19:42:39 +0900

    各modelのアソシエーションを記述

--- app/models/address_book.rb
+++ app/models/address_book.rb
@@ -1,2 +1,3 @@
 class AddressBook < ApplicationRecord
+  belongs_to :order
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,5 @@
 class Order < ApplicationRecord
+  has_one :address_book
+  belongs_to :user
+  belongs_to :item
 end
