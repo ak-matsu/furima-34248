b8269e609c0bc9b341da63ea8e50f0ff874baab0
ak-matsu <sandbox.matsu@gmail.com>
2021-05-09 20:05:43 +0900

    itemコントローラーのnew,createアクション追加によりroutesも編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,17 @@ class ItemsController < ApplicationController
   def index
     @items = Item.all
   end
+  
+  def new
+    @item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :image, :text)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   root to: "items#index"
-  resources :items,only: :index
+  resources :items,only: [:index, :new, :create]
 end
