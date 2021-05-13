380471bddf3281b5d429a24ba9d156bd71d3f9c8
ak-matsu <sandbox.matsu@gmail.com>
2021-05-13 23:45:55 +0900

    itメソッドを用いてテストの状況を記述

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'ユーザー新規登録' do
+    it 'nicknameが空では登録できない' do
+      # nicknameが空では登録できないテストコードを記述します
+    end
+    it 'emailが空では登録できない' do
+      # emailが空では登録できないテストコードを記述します
+    end
+  end
 end
