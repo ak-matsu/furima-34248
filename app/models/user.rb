a21b0bad41aae5f0210b396f14f009beeb6ecf6a
ak-matsu <sandbox.matsu@gmail.com>
2021-05-14 00:43:11 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
+  validates :sei_zen,presence: true
+  validates :mei_zen,presence: true
+  validates :sei_kana,presence: true
+  validates :mei_kana,presence: true
+  validates :birthday,presence: true
 end
