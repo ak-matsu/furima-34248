2fa7aba2d556d059a9319b74ce297fc3f29191c6
ak-matsu <sandbox.matsu@gmail.com>
2021-05-15 23:23:05 +0900

    バリデーションでformatを記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,12 @@ class User < ApplicationRecord
 
   with_options presence: true do
     validates :nickname
-    validates :sei_zen
-    validates :mei_zen
-    validates :sei_kana
-    validates :mei_kana
+    validates :sei_zen,format: {with: /\A[ぁ-んァ-ン一-龥]/}
+    validates :mei_zen,format: {with: /\A[ぁ-んァ-ン一-龥]/}
+    validates :sei_kana,format: {with: /\A[ァ-ヶー－]+\z/}
+    validates :mei_kana,format: {with: /\A[ァ-ヶー－]+\z/}
     validates :birthday
   end
+  validates :encrypted_password,:password,:password_confirmation,format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{7,}/}
+
 end
