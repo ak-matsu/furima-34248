af5bcad5f5ad1b6976519ef4dd230598642ffd39
ak-matsu <sandbox.matsu@gmail.com>
2021-05-16 01:47:54 +0900

    Fakerの設定

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :user do
-    nickname              {'test'}
-    email                 {'test@example'}
-    password              {'00000000'}
+    nickname               {Faker::Name.initials(number: 2)}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
     sei_zen               {'山'}
     mei_zen               {'田'}
