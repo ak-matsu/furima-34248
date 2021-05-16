64a89f913f09e9c7e4d038a471c00f3a6b7be8b3
ak-matsu <sandbox.matsu@gmail.com>
2021-05-16 14:09:26 +0900

    英数字混合のパスワード想定できるよう記述

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname               {Faker::Name.initials(number: 2)}
     email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(min_length: 6)}
+    password              {'1a' + Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
     sei_zen               {'山'}
     mei_zen               {'田'}
