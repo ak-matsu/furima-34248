2e8e8c53d1c0ee5ff5dc73c8142bbc5e1b9bf00c
ak-matsu <sandbox.matsu@gmail.com>
2021-05-16 21:34:59 +0900

    Rubocop実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,21 +4,20 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         with_options presence: true do
-          validates :nickname
-          validates :birthday
-      
-          with_options format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/} do
-            validates :sei_zen
-            validates :mei_zen
-          end
-      
-          with_options format: {with: /\A[ァ-ヶー－]+\z/} do
-            validates :sei_kana
-            validates :mei_kana
-          end
-        end
+  with_options presence: true do
+    validates :nickname
+    validates :birthday
 
-  validates :encrypted_password,:password,:password_confirmation,format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{7,}/}
+    with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
+      validates :sei_zen
+      validates :mei_zen
+    end
 
+    with_options format: { with: /\A[ァ-ヶー－]+\z/ } do
+      validates :sei_kana
+      validates :mei_kana
+    end
+  end
+
+  validates :password, :password_confirmation, format: { with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{7,}/ }
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname               {Faker::Name.initials(number: 2)}
-    email                 {Faker::Internet.free_email}
-    password              {'1a' + Faker::Internet.password(min_length: 6)}
-    password_confirmation {password}
-    sei_zen               {'山'}
-    mei_zen               {'田'}
-    sei_kana              {'ヤマ'}
-    mei_kana               {'ダ'}
-    birthday              {'19680301'}
+    nickname { Faker::Name.initials(number: 2) }
+    email                 { Faker::Internet.free_email }
+    password              { '1a' + Faker::Internet.password(min_length: 6) }
+    password_confirmation { password }
+    sei_zen               { '山' }
+    mei_zen               { '田' }
+    sei_kana              { 'ヤマ' }
+    mei_kana { 'ダ' }
+    birthday { '19680301' }
   end
 end
