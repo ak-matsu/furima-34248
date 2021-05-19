29c6b384be28ff0c4fbd6548fcfd6c22db0f1e49
ak-matsu <sandbox.matsu@gmail.com>
2021-05-19 20:17:24 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,12 @@
 class Item < ApplicationRecord
+  with_options presence: true do
+  validates :user
+  validates :text
+  validates :category_id
+  validates :status_id
+  validates :delivery_money_id
+  validates :prefecture_id
+  validates :delivery_day_id
+  validates :price
+  end
 end
