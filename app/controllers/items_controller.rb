116ea376a7e5624e48fbf204d4727e1db5479763
ak-matsu <sandbox.matsu@gmail.com>
2021-05-19 13:03:59 +0900

    createアクションをコントローラーに定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,5 +6,14 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+
+  def create
+    Item.create(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:image,:name,:text,:category_id,:status_id,:delivery_money_id,:prefecture_id,:delivery_day_id,:price)
+  end
   
 end
