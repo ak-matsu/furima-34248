590e7b462022f873569a11bc60ba9264d75b9874
ak-matsu <sandbox.matsu@gmail.com>
2021-05-21 13:05:42 +0900

    投稿機能を実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,7 +9,12 @@ class ItemsController < ApplicationController
   end
 
   def create
-    Item.create(item_params)
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
