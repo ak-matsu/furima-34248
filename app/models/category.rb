4af3269fde2be7655ac0cc89294f5dc34b3c0e2f
ak-matsu <sandbox.matsu@gmail.com>
2021-05-22 22:16:09 +0900

    カンマのつけ忘れ

--- app/models/category.rb
+++ app/models/category.rb
@@ -9,8 +9,8 @@ class Category < ActiveHash::Base
     { id: 7, name: 'おもちゃ・ホビー・グッズ' },
     { id: 8, name: '家電・スマホ・カメラ' },
     { id: 9, name: 'スポーツ・レジャー' },
-    { id: 10, name: 'ハンドメイド' }
-    { id: 11, name: 'その他'}
+    { id: 10, name: 'ハンドメイド' },
+    { id: 11, name: 'その他'},
   ]
 
   include ActiveHash::Associations
