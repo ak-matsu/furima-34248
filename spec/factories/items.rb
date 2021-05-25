e02276e50b6e87bfd0630d5da10434774de2ae9b
ak-matsu <sandbox.matsu@gmail.com>
2021-05-25 13:01:53 +0900

    単体テストの準備、フェイカーの記述

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,37 @@
 FactoryBot.define do
   factory :item do
-    
+    # イメージ
+    image { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/test.jpg'))}
+    # 商品名
+    name                { Faker::Commerce.product_name}
+    # 商品の説明
+
+    # カテゴリー
+
+    # 商品の状態
+
+    # 配送料の負担
+
+    # 販売元の地域
+
+    # 発送までの日数
+
+    # 価格
+
+  end
+end
+
+
+FactoryBot.define do
+  factory :user do
+    nickname { Faker::Name.initials(number: 2) }
+    email                 { Faker::Internet.free_email }
+    password              { '1a' + Faker::Internet.password(min_length: 6) }
+    password_confirmation { password }
+    sei_zen               { '山' }
+    mei_zen               { '田' }
+    sei_kana              { 'ヤマ' }
+    mei_kana { 'ダ' }
+    birthday { '19680301' }
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '商品出品機能' do
+    before do
+      @item = FactoryBot.build(:item)
+      @item.image = fixture_file_upload('app/assets/images/logo.png')
+
+    end
+
+    # 正常テスト
+    context '出品できるとき' do
+      it '出品画像が投稿できるとき' do
+        @item.image = fixture_file_upload('app/assets/images/logo.png')
+        expect(@item).to be_valid
+      end
+
+    end
+
+
+    # 異常テスト
+    context '出品に失敗するとき' do
+
+    end
+  end
 end
