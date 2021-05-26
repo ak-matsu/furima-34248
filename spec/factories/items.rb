b738cd98e00ebb8bddca03d81c6237a64ec4e96d
ak-matsu <sandbox.matsu@gmail.com>
2021-05-26 21:11:16 +0900

    異常テストを記載

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,37 +1,46 @@
 FactoryBot.define do
   factory :item do
     # イメージ
-    image { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/test.jpg'))}
+    # image { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/test.jpg'))}
     # 商品名
+    #  id               { Faker::IDNumber.croatian_id }
+    user_id            {1}
     name                { Faker::Commerce.product_name}
     # 商品の説明
-
+    text                {Faker::Commerce.department}
+    
     # カテゴリー
+    category_id         {1}
 
     # 商品の状態
+    status_id           {1}
 
     # 配送料の負担
+    delivery_money_id   {100}
 
     # 販売元の地域
+    prefecture_id       {1}
 
     # 発送までの日数
+    delivery_day_id     {1}
 
     # 価格
+    price             {300}
 
   end
 end
 
 
-FactoryBot.define do
-  factory :user do
-    nickname { Faker::Name.initials(number: 2) }
-    email                 { Faker::Internet.free_email }
-    password              { '1a' + Faker::Internet.password(min_length: 6) }
-    password_confirmation { password }
-    sei_zen               { '山' }
-    mei_zen               { '田' }
-    sei_kana              { 'ヤマ' }
-    mei_kana { 'ダ' }
-    birthday { '19680301' }
-  end
-end
+# FactoryBot.define do
+#   factory :user do
+#     nickname { Faker::Name.initials(number: 2) }
+#     email                 { Faker::Internet.free_email }
+#     password              { '1a' + Faker::Internet.password(min_length: 6) }
+#     password_confirmation { password }
+#     sei_zen               { '山' }
+#     mei_zen               { '田' }
+#     sei_kana              { 'ヤマ' }
+#     mei_kana { 'ダ' }
+#     birthday { '19680301' }
+#   end
+# end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -2,25 +2,50 @@ require 'rails_helper'
 
 RSpec.describe Item, type: :model do
   describe '商品出品機能' do
-    before do
-      @item = FactoryBot.build(:item)
-      @item.image = fixture_file_upload('app/assets/images/logo.png')
-
-    end
+    # before do
+    #   @item = FactoryBot.build(:item)
+      # @item.image = fixture_file_upload('app/assets/images/logo.png')
+    # end
 
     # 正常テスト
-    context '出品できるとき' do
-      it '出品画像が投稿できるとき' do
-        @item.image = fixture_file_upload('app/assets/images/logo.png')
-        expect(@item).to be_valid
-      end
+    # context '出品できるとき' do
+      # it '出品画像が投稿できるとき' do
+      #    @item.image
+      #    expect(@item).to be_valid
+      # end
 
-    end
+    #   it '商品名が入力される' do
+    #     @item.name
+    #     expect(@item).to be_valid
+    #   end
 
-
+    # end
     # 異常テスト
     context '出品に失敗するとき' do
 
+      it 'imageが空では登録できない'do
+      end
+
+      it '商品名が空では登録できない'do
+      end
+
+      it  '商品説明が空では登録できない' do
+      end
+
+      it  'カテゴリーが空では登録できない' do
+      end
+
+      it  '商品の状態が空では登録できない' do
+      end
+
+      it  '発送元の地域が空では登録できない' do
+      end
+
+      it  '発送までの日数が空では登録できない' do
+      end
+
+      it '価格が空だと登録できない' do
+      end
     end
   end
 end
