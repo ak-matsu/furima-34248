ec7ed8d4fbb1f1fdca0d6b7b77a407c65fb851c5
ak-matsu <sandbox.matsu@gmail.com>
2021-06-05 00:21:47 +0900

    編集成功時、失敗時を作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -29,7 +29,11 @@ class ItemsController < ApplicationController
 
   def update
     item = Item.find(params[:id])
-    item.update(item_params)
+    if item.update(item_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
   end
 
   private
