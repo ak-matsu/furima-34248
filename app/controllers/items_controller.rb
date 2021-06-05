e649f6ffe549de171e17d118e6cf76d3692d4ab5
ak-matsu <sandbox.matsu@gmail.com>
2021-06-05 14:20:03 +0900

    非ログインユーザーが詳細ページからURLを直接打ち込むと編集できないようにコントローラーを記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -25,6 +25,9 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+    unless user_signed_in? && current_user.id == @item.user_id
+      redirect_to action: :index
+    end
   end
 
   def update
