ff2f4066de48163328ae74de1cc0b30e33fd9d57
ak-matsu <sandbox.matsu@gmail.com>
2021-06-10 21:56:12 +0900

    カード情報のjs記述

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,9 +1,18 @@
 const pay = () => {
+  Payjp.setPublicKey("pk_test_******************"); // PAY.JPテスト公開鍵
   const form = document.getElementById("charge-form");
   form.addEventListener("submit", (e) => {
     e.preventDefault();
-    console.log("フォーム送信時にイベント発火")
-  });
+    
+    const formResult = document.getElementById("charge-form");
+    const formData = new FormData(formResult);
+
+    const card = {
+      number: formData.get("order[number]"),
+      cvc: formData.get("order[cvc]"),
+      exp_month: formData.get("order[exp_month]"),
+      exp_year: `20${formData.get("order[exp_year]")}`,
+    };
 };
 
 window.addEventListener("load", pay);
