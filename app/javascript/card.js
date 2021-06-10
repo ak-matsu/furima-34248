01284da91a302e73730673ad0f35cfa8f42d22d4
ak-matsu <sandbox.matsu@gmail.com>
2021-06-10 21:29:14 +0900

    Update card.js

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,9 @@
 const pay = () => {
-  console.log("カード情報トークン化のためのJavaScript");
+  const form = document.getElementById("charge-form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log("フォーム送信時にイベント発火")
+  });
 };
 
 window.addEventListener("load", pay);
