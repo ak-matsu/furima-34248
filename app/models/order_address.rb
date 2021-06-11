9e0c52bc5b5506225b3801aac7fdb597afab9da8
ak-matsu <sandbox.matsu@gmail.com>
2021-06-11 14:25:42 +0900

    不要記述を削除

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,42 +1,27 @@
 class OrderAddress
-
   include ActiveModel::Model
   # orderテーブルとaddressbookテーブルに保存したいカラム名を指定
   attr_accessor :postnum,:prefecture_id,:city,:banchi,:tower,:phone,:user_id,:item_id
-
   # ここにバリデーションの処理を書く
   with_options presence: true do
     validates :user_id
-
     # カード情報
     validates :
-
     # 有効期限
-
-
     # セキュリティコード4桁
-
-
     # 配送先住所
-
-    
     # 郵便番号
     validates :postnum, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
   end
   validates :prefecture, numericality: {other_than: 0, message: "can't be blank"}
-
     # 市町村
     validates :city
-
     # 番地
     validates :banchi
-
     # 建物名
     validates :tower
-
     # 電話番号
     validates :phone
-
   def save
     # 寄付情報を保存し、変数donationに代入する
     order = order.create(item_id: item_id, user_id: user_id)
