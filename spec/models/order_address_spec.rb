7d360f65962cc1b53622af00972de80b88d7a405
ak-matsu <sandbox.matsu@gmail.com>
2021-06-15 13:13:05 +0900

    ５行目６行目に解決策を記述

--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -2,17 +2,22 @@ require 'rails_helper'
 
 describe OrderAddress, type: :model do
 
+# before内に  user,itemをDBに保存する
+# そのid番号を用いて@order_addressを作成する。
+
   before do
     @order_address = FactoryBot.build(:order_address)
   end
 
+  #正常テスト
   describe '商品購入機能' do
     context '内容に問題ない場合' do
-      # it "priceがあれば保存ができること" do
-      #   expect(@orderaddress).to be_valid
-      # end
+      it "商品購入できるとき" do
+        expect(@order_address).to be_valid
+      end
     end
 
+    #異常テスト
     context '内容に問題がある場合' do
       it "postnumが空では保存ができないこと" do
         @order_address.postnum = nil
