ae2570b3ae08b3b47604dfd3bd56f380f9d78679
ak-matsu <sandbox.matsu@gmail.com>
2021-06-17 22:15:23 +0900

    Update card.js

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,25 +1,25 @@
 const pay = () => {
-  // Payjp.setPublicKey("pk_test_******************"); // PAY.JPテスト公開鍵
-  // const form = document.getElementById("charge-form");
-  // form.addEventListener("submit", (e) => {
-  //   e.preventDefault();
+  Payjp.setPublicKey("pk_test_******************"); // PAY.JPテスト公開鍵
+  const form = document.getElementById("charge-form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
 
-  //   const formResult = document.getElementById("charge-form");
-  //   const formData = new FormData(formResult);
+    const formResult = document.getElementById("charge-form");
+    const formData = new FormData(formResult);
 
-  //   const card = {
-  //     number: formData.get("order[number]"),
-  //     cvc: formData.get("order[cvc]"),
-  //     exp_month: formData.get("order[exp_month]"),
-  //     exp_year: `20${formData.get("order[exp_year]")}`,
-  //   };
-  //   Payjp.createToken(card, (status, response) => {
-  //     if (status == 200) {
-  //       const token = response.id;
-  //       console.log(token)
-  //     }
-  //   });
-  // });
+    const card = {
+      number: formData.get("order[number]"),
+      cvc: formData.get("order[cvc]"),
+      exp_month: formData.get("order[exp_month]"),
+      exp_year: `20${formData.get("order[exp_year]")}`,
+    };
+    Payjp.createToken(card, (status, response) => {
+      if (status == 200) {
+        const token = response.id;
+        console.log(token)
+      }
+    });
+  });
 };
 
 window.addEventListener("load", pay);
