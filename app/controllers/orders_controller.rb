3e388896c15f3cfc77834a5ffb3eeac8a2aa42c8
ak-matsu <sandbox.matsu@gmail.com>
2021-06-17 22:41:13 +0900

    jsファイル編集

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -30,4 +30,13 @@ class OrdersController < ApplicationController
   def order_params
     params.permit(:postnum,:prefecture_id,:city,:banchi,:tower,:phone).merge(user_id: current_user.id, item_id: params[:item_id])
   end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: order_params[:price],  # 商品の値段
+      card: order_params[:token],    # カードトークン
+      currency: 'jpy'                 # 通貨の種類（日本円）
+    )
+  end
 end
