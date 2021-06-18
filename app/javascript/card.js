e261c02c9617737f53a743b13d58c079b3967c32
ak-matsu <sandbox.matsu@gmail.com>
2021-06-18 21:01:05 +0900

    送信の挙動を止める文をコメントアウト

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -2,7 +2,7 @@ const pay = () => {
   Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);
   const form = document.getElementById("charge-form");
   form.addEventListener("submit", (e) => {
-    e.preventDefault();
+    // e.preventDefault();
     console.log("フォーム送信時にイベント発火")
 
     const formResult = document.getElementById("charge-form");
