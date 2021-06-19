71ae57c4fcd902c3d4ca9c7145afd91f86169bb9
ak-matsu <sandbox.matsu@gmail.com>
2021-06-19 21:45:39 +0900

    form_withのURL参照先編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
-  get 'orders/create', to: 'orders#create'
-  
+
   resources :items do
-    resources :orders, only: [:index,:create]
+    resources :orders, only: [:index, :create]
   end
 end
