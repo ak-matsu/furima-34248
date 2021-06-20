ec9df3dc36d62981fe2d996a4fcb208fe5365604
ak-matsu <sandbox.matsu@gmail.com>
2021-06-20 15:59:53 +0900

    createアクションへ@itemを記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -18,7 +18,10 @@ class OrdersController < ApplicationController
 
   def create
     @addressbook = Addressbook.new
+    @item = Item.find(params[:item_id])
+
     @order_address = OrderAddress.new(order_params)
+    binding.pry
     if @order_address.valid?
       pay_item
       @order_address.save
@@ -36,8 +39,8 @@ class OrdersController < ApplicationController
   def pay_item
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     Payjp::Charge.create(
-      amount: order_params[:price],  # 商品の値段
-      card: order_params[:token],    # カードトークン
+      amount: @item_params[:price],  # 商品の値段
+      card: @order_params[:token],    # カードトークン
       currency: 'jpy'                 # 通貨の種類（日本円）
     )
   end
