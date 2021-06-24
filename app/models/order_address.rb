a2302872454b9dfac3f53469f5b81b08473cd3df
ak-matsu <sandbox.matsu@gmail.com>
2021-06-25 01:08:30 +0900

    Update order_address.rb

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,7 +1,7 @@
 class OrderAddress
   include ActiveModel::Model
   # orderテーブルとaddressbookテーブルに保存したいカラム名を指定
-  attr_accessor :postnum, :prefecture_id, :city, :banchi, :tower, :phone, :user_id, :item_id,:token
+  attr_accessor :token,:postnum, :prefecture_id, :city, :banchi, :tower, :phone, :user_id, :item_id
 
   # ここにバリデーションの処理を書く
 
@@ -10,7 +10,6 @@ class OrderAddress
     validates :item_id
     validates :token
     validates :postnum, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "Postnum can't be blank" }
-
     validates :city
     validates :banchi
     validates :phone,format: { with: /\A0[5789]0\d{4}\d{4}\z/} 
@@ -23,7 +22,6 @@ class OrderAddress
     order = Order.create(item_id: item_id, user_id: user_id)
     # 住所を保存する
     # order_idには、変数orderのidと指定する
-    Addressbook.create(order_id: order.id, postnum: postnum, prefecture_id: prefecture_id, city: city, banchi: banchi,
-                       tower: tower, phone: phone)
+    Addressbook.create(order_id: order.id, postnum: postnum, prefecture_id: prefecture_id, city: city, banchi: banchi,tower: tower, phone: phone)
   end
 end
