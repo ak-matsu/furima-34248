fd1bf7e9e559529cbba5ba3c5e91f86b61ef84f2
ak-matsu <sandbox.matsu@gmail.com>
2021-06-24 22:11:01 +0900

    単体テスト用のダミートークン情報作成、towerは空白でも作れるので項目を削除

--- spec/factories/order_addresses.rb
+++ spec/factories/order_addresses.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
   factory :order_address do
+    token {"tok_abcdefghijk00000000000000000"}
     postnum { '123-4567' }
     prefecture_id { 2 }
     city { '横浜' }
     banchi { '青山1−1−1' }
-    tower { '柳ビル103' }
     phone { '09012345678' }
   end
 end
