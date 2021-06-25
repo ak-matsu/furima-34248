4bc6d59d7afda11f6903edacea81bcfafa347f72
ak-matsu <sandbox.matsu@gmail.com>
2021-06-25 20:51:36 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   # ログインしていないユーザーをログインページの画面に促すことができる。
-  before_action :authenticate_user!, only: [:new, :edit, :destroy, :show]
+  before_action :authenticate_user!, only: [:new, :edit, :destroy]
 
   # before_action :処理させたいメソッド名 only:オプション[:httpメソッド]
   before_action :set_item, only: [:edit, :show, :update, :destroy]
