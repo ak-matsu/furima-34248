a288dd9d35f3d95bfbc5c9f1b670bc2b46d99a90
ak-matsu <sandbox.matsu@gmail.com>
2021-06-25 20:28:26 +0900

    トークン情報を預ける

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -3,7 +3,9 @@ const pay = () => {
   Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);
   const form = document.getElementById("charge-form");
   form.addEventListener("submit", (e) => {
- 
+    e.preventDefault();
+
+
     const formResult = document.getElementById("charge-form");
     const formData = new FormData(formResult);    
     const card = {
@@ -12,19 +14,23 @@ const pay = () => {
       exp_month: formData.get("order_address[exp_month]"),
       exp_year: `20${formData.get("order_address[exp_year]")}`,
     };
+
     Payjp.createToken(card, (status, response) => {
       if (status == 200) {
         const token = response.id;
         const renderDom = document.getElementById("charge-form");
         const tokenObj = `<input value=${token} name='token'  type="hidden">`;
         renderDom.insertAdjacentHTML("beforeend", tokenObj);
-        debugger;
+        // debugger;
       }
       document.getElementById("card-number").removeAttribute("name");
       document.getElementById("card-exp-month").removeAttribute("name");
       document.getElementById("card-exp-year").removeAttribute("name");
       document.getElementById("card-cvc").removeAttribute("name");
+      
       document.getElementById("charge-form").submit();
+      document.getElementById("charge-form").reset();
+
     });
   });
 };
