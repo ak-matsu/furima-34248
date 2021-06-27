e5a9caecfffefdf80ef34c969be73e2ae0738432
ak-matsu <sandbox.matsu@gmail.com>
2021-06-27 22:15:07 +0900

    up

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,11 +1,11 @@
 #サーバ上でのアプリケーションコードが設置されているディレクトリを変数に入れておく
-app_path = File.expand_path('../../../', __FILE__)
+app_path = File.expand_path('../../../', __FILE__) 
 
 #アプリケーションサーバの性能を決定する
 worker_processes 1
 
 #アプリケーションの設置されているディレクトリを指定
-working_directory "#{app_path}/current"
+working_directory "#{app_path}/current" 
 
 #Unicornの起動に必要なファイルの設置場所を指定
 pid "#{app_path}/shared/tmp/pids/unicorn.pid"
@@ -14,7 +14,7 @@ pid "#{app_path}/shared/tmp/pids/unicorn.pid"
 listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
 
 #エラーのログを記録するファイルを指定
-stderr_path "#{app_path}/shared/log/unicorn.stderr.log" 
+stderr_path "#{app_path}/shared/log/unicorn.stderr.log"  
 
 #通常のログを記録するファイルを指定
 stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
